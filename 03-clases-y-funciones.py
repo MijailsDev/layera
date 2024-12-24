0c3c2e9226b478815f19beaf6ff77d6348043eb4
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-24 09:15:54 -0500

    Definir nuevos metodos

---
+++ 03-clases-y-funciones.py
@@ -0,0 +1,26 @@
+# Creacion de mas Metodos
+class Helado:
+
+    # Definir Atributos 
+    def __init__(self, sabor, tamano, tipo_envase):
+
+        self.sabor = sabor
+        self.tamano = tamano
+        self.tipo_envase = tipo_envase 
+        
+    
+    # Definir Metodos
+    def derretir(self):
+        print(f"El helado de {self.sabor} de tamaño {self.tamano} y tipo de envase {self.tipo_envase} se derrite")
+
+    def congelar(self):
+        print(f"El helado de {self.sabor} de tamaño {self.tamano} y tipo de envase {self.tipo_envase} se congela")
+
+    def saborear(self):
+        print(f"Yo saboreo el helado de {self.sabor} de tamaño {self.tamano} y tipo de envase {self.tipo_envase}")
+
+heladito = Helado("Chocolate", "pequeno", "cono")
+helado1 = Helado("Fresa", "grande", "vasito")
+
+heladito.derretir()
+helado1.derretir()
